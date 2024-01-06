feed274e6aa2798986828634d747693f26d3bab0
阿部英行 <hide@abehideyukinoMacBook-Air.local>
2024-01-06 14:35:31 +0900

    1/6

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists/create'
   get 'lists' => 'lists#index'
-  get 'list/:id' => 'lists#show'
-  get 'lists/edit'
+  get 'list/:id' => 'lists#show', as: 'list'
+  get 'lists/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
 end

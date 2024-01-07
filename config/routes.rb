10995b7697607aa939c7dcdb1471a0a6b2b7adee
阿部英行 <vandaru3@gmail.com>
2024-01-07 14:11:33 +0900

    1/7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists/create'
-  get 'lists' => 'lists#index'
-  get 'list/:id' => 'lists#show', as: 'list'
-  get 'lists/edit' => 'lists#edit', as: 'edit_list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
-end
+  ge
